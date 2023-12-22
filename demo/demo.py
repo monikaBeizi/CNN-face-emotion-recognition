2b5890d378fd71dfa9d80efade66e20f184c2c97
lunshenghan <87764588+l550084743@users.noreply.github.com>
2023-12-22 16:16:45 +0800

    多次进行vgg，取数量最多的标签

--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sys
 import cv2
+from PIL import Image
 from ultralytics import YOLO
 
 sys.path.append('.')
@@ -20,6 +21,7 @@ if __name__ == '__main__':
     cap = cv2.VideoCapture(0)  # 0表示默认摄像头，如果有多个摄像头，可以尝试使用1, 2, 等
 
     # 遍历视频帧
+
     while cap.isOpened():
         # 从视频中读取一帧
         success, frame = cap.read()
@@ -28,23 +30,22 @@ if __name__ == '__main__':
             # 在该帧上运行YOLOv8推理
             results = yolo(frame)
 
-            # img = Image.fromarray(frame, )
-            #
-            # img.show()
-
             result = results[0]
 
             imgs = cut_image(result)
-
             labels = []
-
             for i in imgs:
-                pre_image = trans_predict(i)
-                ans = model.predict(pre_image)
-                labels.append(get_emotions(ans))
+                most = []
+                for n in range(7):
+                    pre_image = trans_predict(i)
+                    ans = model.predict(pre_image)
+                    most.append(ans)
+                print(most)
+                most_num = max(most, key=most.count)
+                labels.append(get_emotions(most_num))
 
             # 在帧上可视化结果
-
+            print(labels)
             annotated_frame = plot_boxes(labels, result)
 
             # 显示带注释的帧
@@ -53,20 +54,7 @@ if __name__ == '__main__':
             # 如果按下'q'则中断循环
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
-        # else:
-        #     # 如果视频结束则中断循环
-        #     break
 
     # 释放视频捕获对象并关闭显示窗口
     cap.release()
     cv2.destroyAllWindows()
-
-    #
-    #
-    #
-    # grey_image = np.array(grey_image)
-    #
-    # pre_image= trans_predict(grey_image)
-    # ans = model.predict(pre_image)
-    #
-    # print(get_emotions(ans))
